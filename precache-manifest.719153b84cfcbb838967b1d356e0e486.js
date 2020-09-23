29237519858c8da2f84c44c2f6e0926d49f95dfe
Alexander Celis <dacelis0@misena.edu.co>
2020-09-23 16:15:25 -0500

    Updates

---
+++ precache-manifest.719153b84cfcbb838967b1d356e0e486.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d74f0358681b29e4ef803a96b81314",
+    "url": "/agency-react/index.html"
+  },
+  {
+    "revision": "3384241632392fe05b56",
+    "url": "/agency-react/static/css/main.896dc85e.chunk.css"
+  },
+  {
+    "revision": "dea3cafc4894dfadf9f0",
+    "url": "/agency-react/static/js/2.b789a387.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/agency-react/static/js/2.b789a387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3384241632392fe05b56",
+    "url": "/agency-react/static/js/main.ad5e8dba.chunk.js"
+  },
+  {
+    "revision": "027da0c8347d23d7cbb4",
+    "url": "/agency-react/static/js/runtime-main.f3f696b9.js"
+  },
+  {
+    "revision": "bef2041cd8f602a4a60fb4ea76e8c039",
+    "url": "/agency-react/static/media/destinyJardinNegro.bef2041c.png"
+  },
+  {
+    "revision": "36865cce32dd68885d761fc9a68391ba",
+    "url": "/agency-react/static/media/destinyJardinNegroMobile.36865cce.png"
+  },
+  {
+    "revision": "3bbda4d3a4d5c26369edbc1322ad4ec8",
+    "url": "/agency-react/static/media/destinyJardinNegroTablet.3bbda4d3.png"
+  },
+  {
+    "revision": "47ee84c7313dfcad432885b803af7d8f",
+    "url": "/agency-react/static/media/logo.47ee84c7.svg"
+  }
+]);
